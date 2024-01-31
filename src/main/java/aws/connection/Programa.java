09999064d3530125d6d6c8ca59ce81a603276bf4
DAdB03-design <128997278+DAdB03-design@users.noreply.github.com>
2024-01-31 20:43:26 +0100

    31/20:43
    INSETADA LOGICA DE PROGRAMA SIN ADAPTAR

--- src/main/java/aws/connection/Programa.java
+++ src/main/java/aws/connection/Programa.java
@@ -1,11 +1,196 @@
 package aws.connection;
 
-
+import java.util.Scanner;
 
 public class Programa {
 	public static void main(String[] args) {
+		  Scanner scanner = new Scanner(System.in);
+	        int opcion;
 
+	        do {
+	            System.out.println("Menú Principal:");
+	            System.out.println("1. Importar datos");
+	            System.out.println("2. Exportar datos");
+	            System.out.println("3. Insertar registro");
+	            System.out.println("4. Eliminar registro");
+	            System.out.println("5. Editar registro");
+	            System.out.println("6. Consultar registro");
+	            System.out.println("7. Consultar tablas");
+	            System.out.println("8. Salir");
+	            System.out.print("Seleccione una opción: ");
+
+	            opcion = scanner.nextInt();
+
+	            if (opcion == 1) {
+	                int subopcionImportar;
+	                do {
+	                    System.out.println("Submenú de Importar Datos:");
+	                    System.out.println("1. Importar datos de libros desde JSON");
+	                    System.out.println("2. Importar datos de ventas desde JSON");
+	                    System.out.println("3. Importar datos de empleados desde JSON");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionImportar = scanner.nextInt();
+
+	                    if (subopcionImportar == 1) {
+	                        // Implementa la lógica para importar datos de libros desde JSON
+	                    } else if (subopcionImportar == 2) {
+	                        // Implementa la lógica para importar datos de ventas desde JSON
+	                    } else if (subopcionImportar == 3) {
+	                        // Implementa la lógica para importar datos de empleados desde JSON
+	                    } else if (subopcionImportar == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionImportar != 4);
+	            } else if (opcion == 2) {
+	                // Implementa la lógica para exportar datos a JSON
+	            } else if (opcion == 3) {
+	                int subopcionInsertar;
+	                do {
+	                    System.out.println("Submenú de Insertar Registro:");
+	                    System.out.println("1. Insertar libro");
+	                    System.out.println("2. Insertar venta");
+	                    System.out.println("3. Insertar empleado");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionInsertar = scanner.nextInt();
+
+	                    if (subopcionInsertar == 1) {
+	                        // Implementa la lógica para insertar un libro
+	                    } else if (subopcionInsertar == 2) {
+	                        // Implementa la lógica para insertar una venta
+	                    } else if (subopcionInsertar == 3) {
+	                        // Implementa la lógica para insertar un empleado
+	                    } else if (subopcionInsertar == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionInsertar != 4);
+	            } else if (opcion == 4) {
+	                int subopcionEliminar;
+	                do {
+	                    System.out.println("Submenú de Eliminar Registro:");
+	                    System.out.println("1. Eliminar libro");
+	                    System.out.println("2. Eliminar venta");
+	                    System.out.println("3. Eliminar empleado");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionEliminar = scanner.nextInt();
+
+	                    if (subopcionEliminar == 1) {
+	                        // Implementa la lógica para eliminar un libro
+	                    } else if (subopcionEliminar == 2) {
+	                        // Implementa la lógica para eliminar una venta
+	                    } else if (subopcionEliminar == 3) {
+	                        // Implementa la lógica para eliminar un empleado
+	                    } else if (subopcionEliminar == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionEliminar != 4);
+	            } else if (opcion == 5) {
+	                int subopcionEditar;
+	                do {
+	                    System.out.println("Submenú de Editar Registro:");
+	                    System.out.println("1. Editar libro");
+	                    System.out.println("2. Editar venta");
+	                    System.out.println("3. Editar empleado");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionEditar = scanner.nextInt();
+
+	                    if (subopcionEditar == 1) {
+	                        // Implementa la lógica para editar un libro
+	                    } else if (subopcionEditar == 2) {
+	                        // Implementa la lógica para editar una venta
+	                    } else if (subopcionEditar == 3) {
+	                        // Implementa la lógica para editar un empleado
+	                    } else if (subopcionEditar == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionEditar != 4);
+	            } else if (opcion == 6) {
+	                int subopcionConsultarRegistro;
+	                do {
+	                    System.out.println("Submenú de Consultar Registro:");
+	                    System.out.println("1. Consultar libro");
+	                    System.out.println("2. Consultar venta");
+	                    System.out.println("3. Consultar empleado");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionConsultarRegistro = scanner.nextInt();
+
+	                    if (subopcionConsultarRegistro == 1) {
+	                        // Implementa la lógica para consultar un libro
+	                    } else if (subopcionConsultarRegistro == 2) {
+	                        // Implementa la lógica para consultar una venta
+	                    } else if (subopcionConsultarRegistro == 3) {
+	                        // Implementa la lógica para consultar un empleado
+	                    } else if (subopcionConsultarRegistro == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionConsultarRegistro != 4);
+	            } else if (opcion == 7) {
+	                int subopcionConsultarTablas;
+	                do {
+	                    System.out.println("Submenú de Consultar Tablas:");
+	                    System.out.println("1. Consultar tabla de libros");
+	                    System.out.println("2. Consultar tabla de ventas");
+	                    System.out.println("3. Consultar tabla de empleados");
+	                    System.out.println("4. Volver al menú principal");
+	                    System.out.print("Seleccione una opción: ");
+
+	                    subopcionConsultarTablas = scanner.nextInt();
+
+	                    if (subopcionConsultarTablas == 1) {
+	                        // Implementa la lógica para consultar la tabla de libros
+	                    } else if (subopcionConsultarTablas == 2) {
+	                        // Implementa la lógica para consultar la tabla de ventas
+	                    } else if (subopcionConsultarTablas == 3) {
+	                        // Implementa la lógica para consultar la tabla de empleados
+	                    } else if (subopcionConsultarTablas == 4) {
+	                        System.out.println("Volviendo al menú principal...");
+	                    } else {
+	                        System.out.println("Opción no válida");
+	                    }
+	                } while (subopcionConsultarTablas != 4);
+	            } else if (opcion == 8) {
+	                System.out.println("¡Hasta luego!");
+	            } else {
+	                System.out.println("Opción no válida");
+	            }
+	        } while (opcion != 8);
+
+	        scanner.close();
+	    
 		
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//==========================================================================================================================================================================================	
 		//________________________IMPORTAR DATOS DE JSON_________________________ OK (FALTA VENTAS)
 		
 		
